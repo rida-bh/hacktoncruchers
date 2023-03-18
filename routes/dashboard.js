a6c0e0e507c09f32a1216c1ae55b8aba1a632e1b
Your Name <you@example.com>
2023-03-18 18:44:34 +0100

    mycommit2

---
+++ routes/dashboard.js
@@ -0,0 +1,28 @@
+/*
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
+
+// GET /users - get all users
+router.get('/', usersController.index);
+
+// GET /users/new - display the form for creating a new user
+router.get('/new', usersController.new);
+
+// POST /users - create a new user
+router.post('/', usersController.create);
+
+// GET /users/:id - display details for a specific user
+router.get('/:id', usersController.show);
+
+// GET /users/:id/edit - display the form for editing a specific user
+router.get('/:id/edit', usersController.edit);
+
+// PUT /users/:id - update a specific user
+router.put('/:id', usersController.update);
+
+// DELETE /users/:id - delete a specific user
+router.delete('/:id', usersController.delete);
+
+module.exports = router;
+*/
